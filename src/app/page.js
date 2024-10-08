56ae6caa97f4d1e8550bb386a74cb3347241bb4b
Sachinattri001 <kittusharma0131@gmail.com>
2024-10-08 20:58:58 +0530

    noise-testure-img-error-solved

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import FAB from "@/components/common/FAB";
 import Header from "@/components/common/Header";
 import Hero from "@/components/Hero";
@@ -12,22 +12,23 @@ import Footer from "@/components/common/Footer";
 import Image from "next/image";
 
 export default function Home() {
-
   return (
     <section className="w-full">
       <div className="w-full max-w-[1920px] mx-auto relative overflow-hidden">
         <div className="absolute top-0 left-0 w-full h-full z-[99999] pointer-events-none">
-        <Image
-          src="/images/noise-texture-bg.webp"
-          alt="Noise & Texture-img"
-          layout="fill"
-          objectFit="cover"
-          priority
-          className="!pointer-events-none "
-        />
+          <Image
+            src="/images/png/Noise-Texture.webp"
+            alt="Noise & Texture-img"
+            layout="fill"
+            objectFit="cover"
+            priority
+            className="!pointer-events-none "
+          />
         </div>
-      
-        <div className="hidden lg:flex fixed 3xl:absolute right-3 sm:right-5 top-[50vh] translate-y-[-50%] z-50"><FAB /></div>
+
+        <div className="hidden lg:flex fixed 3xl:absolute right-3 sm:right-5 top-[50vh] translate-y-[-50%] z-50">
+          <FAB />
+        </div>
         <Header />
         <Hero />
         <WhatIsFungora />
